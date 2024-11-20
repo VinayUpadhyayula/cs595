8994c7268f75670c01d1ab7205bd1c42a05d6700
vupadhyayula8 <vupadhyayula@binghamton.edu>
2024-11-20 15:37:04 -0500

    basic unit test case

--- analysis-core/sentiment/tests.py
+++ analysis-core/sentiment/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
+import json
 
-# Create your tests here.
+class SentimentAnalysisTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('predict-sentiment')  #URL name from urlpatterns
+        self.valid_text = {'text': 'This is a great day!'}
+
+    def test_sentiment_analysis_text(self):
+        """Test sentiment prediction with valid input text."""
+        response = self.client.post(
+            self.url,
+            data=json.dumps(self.valid_text),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('prediction', response.json())  # Ensure 'prediction' is in the response
